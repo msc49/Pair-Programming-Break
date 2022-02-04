1bd446a0c062c33708a1666a1b30255e7b909d86
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-02-04 06:23:07 +0000

    make computer sleep for 1.5 hours

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ import time
 #so lets have two breaks during that time
 
 BREAKS = 1
+# we use the range function which includes zero, thats why it is set to one and not two
+# we have a total of two breaks every 1.5hours or 5400 seconds
+for i in range(BREAKS):
+  time.sleep(90*60) # computer asleep for 1.5 hours until we need to open browser
